55a2c2261b35f6bfb00eaeb8aaddadb457b05a8a
João Victor Meneses <joao.meneses@take.net>
2025-05-09 15:13:11 -0300

    removed non used variables

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,7 +5,6 @@ const environment = {
   baseUrl: process.env.BASE_URL || 'http://localhost:3000',
   volumePath: process.env.VOLUME_PATH || path.join(__dirname, '../../../uploads'),
   authToken: process.env.AUTH_TOKEN,
-  nodeEnv: process.env.NODE_ENV || 'development',
   logLevel: process.env.LOG_LEVEL || 'info'
 };
 
