9028a1cd5f26bd80710f3dd11b5a367bc44842a5
João Victor Meneses <joao.meneses@take.net>
2025-05-09 15:33:59 -0300

    fix env variables

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,10 @@
 const path = require('path');
+require('dotenv').config();
 
 const environment = {
   port: process.env.PORT || 3000,
   baseUrl: process.env.BASE_URL || 'http://localhost:3000',
-  volumePath: process.env.VOLUME_PATH || path.join(__dirname, '../../../uploads'),
+  volumePath: process.env.VOLUME_PATH || path.join(__dirname, '../uploads'),
   authToken: process.env.AUTH_TOKEN,
   logLevel: process.env.LOG_LEVEL || 'info'
 };
